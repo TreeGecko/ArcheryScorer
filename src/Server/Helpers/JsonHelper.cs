4897e4e66084dbb98b6207aa14029d89d87fb5cd
Brian Nelson <brian@treegecko.com>
2015-02-20 03:39:56 +0000

    saved

--- src/Server/Helpers/JsonHelper.cs
+++ src/Server/Helpers/JsonHelper.cs
@@ -32,11 +32,41 @@ namespace TreeGecko.Archery.Server.Helpers
             return jShooters;
         }
 
-        public static void PersistCompetition(Competition _jCompetition,
+        public static void PersistCompetition(Guid _accountGuid, 
+            Competition _jCompetition,
             ArcheryScorerManager _manager)
         {
             //Persist Competition
             Guid guid = ParseOrNewGuid(_jCompetition.Guid);
+            Library.Archery.Objects.Competition competition = _manager.GetCompetition(guid);
+            if (competition == null)
+            {
+                competition = new Library.Archery.Objects.Competition
+                {
+                    Guid = guid,
+                    Active = true,
+                    ParentGuid = _accountGuid
+                };
+            }
+            competition.ArrowsPerEnd = _jCompetition.ArrowsPerEnd;
+
+            DateTime dateTime;
+            if (DateTime.TryParse(_jCompetition.Date, out dateTime))
+            {
+                competition.Date = dateTime;
+            }
+            else
+            {
+                competition.Date = DateTime.Now.Date;
+            }
+
+            competition.Ends = _jCompetition.Ends;
+            competition.MaxPointsPerArrow = _jCompetition.MaxPointsPerArrow;
+            competition.Name = _jCompetition.Name;
+            competition.Organization = _jCompetition.Organization;
+            competition.Rounds = _jCompetition.Rounds;
+            competition.TrackX = _jCompetition.TrackX;
+            _manager.Persist(competition);
 
             //PersistCompetitionShoots
             foreach (CompetitionShooter jCompetitionShooter in _jCompetition.CompetitionShooters)
@@ -51,20 +81,58 @@ namespace TreeGecko.Archery.Server.Helpers
         {
             //Persist Competition Shooters
             Guid guid = ParseOrNewGuid(_jCompetitionShooter.Guid);
+            Library.Archery.Objects.CompetitionShooter shooter = _manager.GetCompetitionShooter(guid);
+
+            if (shooter == null)
+            {
+                shooter = new Library.Archery.Objects.CompetitionShooter
+                {
+                    Active = true,
+                    Guid = guid,
+                    ParentGuid = _competitionGuid
+                };
+            }
+
+            shooter.Notes = _jCompetitionShooter.Notes;
+
+            Guid shooterGuid;
+            if (Guid.TryParse(_jCompetitionShooter.ShooterGuid, out shooterGuid))
+            {
+                shooter.ShooterGuid = shooterGuid;
+            }
+
+            shooter.Target = _jCompetitionShooter.Target;
+            _manager.Persist(shooter);
 
             //Persist Rounds
             foreach (Round jRound in _jCompetitionShooter.Rounds)
             {   
-                PersistCompetitionShooter(guid, jRound, _manager);
+                PersistRound(guid, jRound, _manager);
             }
         }
 
-        public static void PersistCompetitionShooter(Guid _competitionShooterGuid, 
+        public static void PersistRound(Guid _competitionShooterGuid, 
             Round _jRound,
             ArcheryScorerManager _manager)
         {
             //Persist Round
             Guid guid = ParseOrNewGuid(_jRound.Guid);
+            Library.Archery.Objects.Round round = _manager.GetRound(guid);
+
+            if (round == null)
+            {
+                round = new Library.Archery.Objects.Round
+                {
+                    Active = true,
+                    Guid = guid,
+                    ParentGuid = _competitionShooterGuid
+                };
+            }
+
+            round.Notes = _jRound.Notes;
+            round.RoundScore = _jRound.RoundScore;
+            round.Sequence = _jRound.Sequence;
+            _manager.Persist(round);
 
             //Persist Ends
             foreach (End jEnd in _jRound.Ends)
@@ -79,6 +147,20 @@ namespace TreeGecko.Archery.Server.Helpers
         {
             //Persist End
             Guid guid = ParseOrNewGuid(_jEnd.Guid);
+            Library.Archery.Objects.End end = _manager.GetEnd(guid);
+            if (end == null)
+            {
+                end = new Library.Archery.Objects.End
+                {
+                    Active = true, 
+                    Guid = _roundGuid, 
+                    ParentGuid = _roundGuid
+                };
+            }
+            end.EndScore = _jEnd.EndScore;
+            end.Notes = _jEnd.Notes;
+            end.Sequence = _jEnd.Sequence;
+            _manager.Persist(end);
 
             //Persist Arrows
             foreach (Arrow jArrow in _jEnd.Arrows)
@@ -92,6 +174,22 @@ namespace TreeGecko.Archery.Server.Helpers
             ArcheryScorerManager _manager)
         {
             //Persist Arrow
+            Guid guid = ParseOrNewGuid(_jArrow.Guid);
+            Library.Archery.Objects.Arrow arrow = _manager.GetArrow(guid);
+            if (arrow == null)
+            {
+                arrow = new Library.Archery.Objects.Arrow
+                {
+                    Active = true, 
+                    Guid = guid, 
+                    ParentGuid = _endGuid
+                };
+            }
+            arrow.IsX = _jArrow.IsX;
+            arrow.Score = _jArrow.Score;
+
+            _manager.Persist(arrow);
+
         }
 
 
